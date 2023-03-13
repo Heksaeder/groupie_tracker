d86636d79cfb610dcb4adcdb8ed6eacad911c957
Céline <celine.kurtaran@gmail.com>
2023-03-13 11:32:43 +0100

    update script to new folder name

--- GroupieTracker/script.sh
+++ GroupieTracker/script.sh
@@ -2,6 +2,6 @@ cd /
 cd 'C:\Program Files\MySQL\MySQL Server 8.0\bin'
 ./mysql --user="root" --database="groupes" -t --execute="SELECT name AS 'Band Name', creationDate AS 'Creation Date', firstAlbum AS 'First Album' FROM groupes;"
 read
-cd 'C:\wamp64\www\Groupie_Tracker'
+cd 'C:\wamp64\www\GroupieTracker'
 go run main.go
 read
